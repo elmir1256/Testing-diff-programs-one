b7f6b1ca8dd21e75aff17ee8da8791c6d95f7ce3
Elmir Mammadov <elmirstan@gmail.com>
2024-11-02 14:48:28 +0400

    Complete whole test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,10 +3,33 @@ import org.junit.jupiter.api.Test;
 
 
 public class CalculatorTest {
+    Calculator calculator = new Calculator();
     @Test
     public void testAdd(){
-        Calculator calculator = new Calculator();
         int result = calculator.add(3, 5);
         Assertions.assertEquals(8, result);
+    }
+
+    @Test
+    public void testSubtract(){
+        int result = calculator.subtract(10, 4);
+        Assertions.assertEquals(6, result);
+    }
+
+    @Test
+    public void testMultiply(){
+        int result = calculator.multiply(20, 3);
+        Assertions.assertEquals(60, result);
+    }
+
+    @Test
+    public void testDivide(){
+        double result = calculator.divide(6, 3);
+        Assertions.assertEquals(2, result);
+    }
+
+    @Test
+    public void testDivideZero(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(3, 0));
     }
 }
